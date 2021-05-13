223e7d5bd8d60d04b59ba93d574321133bca7379
reudismam@gmail.com <reudismam@gmail.com>
2021-05-13 05:54:14 -0300

    player working.

--- src/context/HomeContext.tsx
+++ src/context/HomeContext.tsx
@@ -3,6 +3,15 @@ import { createContext, MutableRefObject, ReactNode, useEffect, useRef, useState
 interface HomeContextData {
     videoRef: MutableRefObject<HTMLVideoElement>;
     video: string;
+    isPlaying: boolean;
+    isMute: boolean;
+    volume: number;
+    currentTime: number;
+    totalTime: number;
+    toonglePlayPause: ()=>void;
+    configMute: ()=> void;
+    configVolume: (volume: number)=> void;
+    configCurrentTime: (time: number) => void;
 }
 
 export const HomeContext = createContext({} as HomeContextData);
@@ -12,22 +21,102 @@ interface HomeContextProviderProps {
 }
 
 export const HomeContextProvider = ({children}:HomeContextProviderProps) => {
-    const [video, setVideo] = useState<string>("");
     const videoRef = useRef<HTMLVideoElement>(null);
-    
+    const [video, setVideo] = useState<string>("");
+    const [volume, setVolume] = useState<number>(1);
+    const [currentTime, setCurrentTime] = useState(0);
+    const [totalTime, setTotalTime] = useState(1);
+    const [lastVolume, setLastVolume] = useState<number>(1);
+    const [isPlaying, setIsPlaying] = useState<boolean>(false);
+    const [isMute, setIsMute] = useState<boolean>(false);
+
     useEffect(()=>{
         configVideo("./videos/video.mp4");
     }, []);
 
-    const configVideo = (video:string) => {
-        setVideo(video);
+    const configVideo = (videoUrl:string) => {
+        setVideo(videoUrl);
+
+        const video = videoRef.current;
+        
+        video.onloadedmetadata = () => {
+            setTotalTime(video.duration);
+        }
+    }
+
+    const toonglePlayPause = () => {
+        const updatedIsPlaying = !isPlaying;
+        if (isPlaying) {
+            pause();
+        }
+        else {
+            play();
+        }
+        setIsPlaying(updatedIsPlaying);
+    }
+
+    const configMute = () => {
+        const updatedMute = !isMute;
+        const video = videoRef.current;
+        video.muted = updatedMute;
+        setIsMute(updatedMute);
+        if (updatedMute) {
+            setLastVolume(volume);
+            video.volume = 0;
+            setVolume(0);
+        }
+        else {
+            video.volume = lastVolume;
+            setVolume(lastVolume);
+        }
+    }
+
+    const configCurrentTime = (time: number) => {
+        const video = videoRef.current;
+        video.currentTime = time;
+        setCurrentTime(time);
+    }
+
+    const configVolume = (value: number) => {
+        const video = videoRef.current;
+        video.volume = value;
+        setLastVolume(volume);
+        setVolume(value);
+        if (value === 0) {
+            video.muted = true;
+            setIsMute(true);
+
+        }
+        else {
+            video.muted = false;
+            setIsMute(false);
+        }
+    }
+
+    const play = ()=> {
+        const video = videoRef.current;
+        video.play();
+    }
+
+    const pause = () => {
+        const video = videoRef.current;
+        video.pause();
     }
 
     return (
         <HomeContext.Provider value={
             {
                 videoRef,
-                video
+                video,
+                isPlaying,
+                isMute,
+                volume,
+                currentTime,
+                totalTime,
+                toonglePlayPause,
+                configMute,
+                configVolume,
+                configCurrentTime
             }
         }>
         {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,58 @@ import { Icon } from '@material-ui/core';
 import { useContext } from 'react'
 import { HomeContext } from '../context/HomeContext';
 import styles from '../styles/Home.module.css';
-import {AiFillStar} from 'react-icons/ai';
 
 export default function Home() {
   const {
     videoRef,
-    video
+    video,
+    isPlaying,
+    isMute,
+    volume,
+    currentTime,
+    totalTime,
+    toonglePlayPause,
+    configMute,
+    configVolume,
+    configCurrentTime
   } = useContext(HomeContext);
   return (
     <div className={styles.container}>
         <div className={styles.video}>
           <video className={styles.conteudo} src={video} ref={videoRef}></video>
-
+           
+          <div className={styles.timeContainer}>
+            <input 
+              className={styles.time}
+              type="range"
+              min={0}
+              max={totalTime}
+              value={currentTime}
+              onChange={e => configCurrentTime(Number(e.target.value))}
+              />
+          </div>
+          
           <div className={styles.playControls}>
-                <Icon className={styles.playIcon}>play_circle_filled</Icon>
-                <div className={styles.time}>
+                { isPlaying ?
+                  (<Icon className={styles.playIcon} onClick={toonglePlayPause} >pause_circle_filled</Icon>)
+                  :
+                  (<Icon className={styles.playIcon} onClick={toonglePlayPause} >play_circle_filled</Icon>)
+                } 
+                { isMute ?
+                  (<Icon className={styles.muteIcon} onClick={configMute}>volume_off</Icon>)
+                  :
+                  (<Icon className={styles.muteIcon} onClick={configMute}>volume_up</Icon>)
+                }
+                <input className={styles.volumeControl}
+                  type="range"
+                  min="0"
+                  max="1"
+                  step="0.01"
+                  value={volume}
+                  onChange={e => configVolume(Number(e.target.value))}
+                  >
+                </input>
+                <div className={styles.timeTotal}>
                   <span>00:00</span> / <span>10:00</span>
                 </div>
           </div>
